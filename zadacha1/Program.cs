82e93c9065e5e0c3a25401146d80543320bc7b2a
unknown <butwhyilove@gmail.com>
2023-03-21 19:58:37 +0300

    создание метода для поиска нужных элементов массива

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -5,3 +5,15 @@
 Console.Clear();
 string[] array1 = new string[4] { "hello", "2", "world", ":-)" };
 string[] array2 = new string[array1.Length];
+void GetSecondArrayWith3symbolOnly(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
